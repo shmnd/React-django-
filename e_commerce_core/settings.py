8bcba5cdf6084c20a4e9ffdfb71090f8fff2443a
Shamnad <hamzashamnad123@gmail.com>
2025-10-20 16:34:05 +0530

    settuped swagger in main url

--- e_commerce_core/settings.py
+++ e_commerce_core/settings.py
@@ -149,7 +149,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
+STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'static'),
+)
+
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT =  os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -158,3 +166,18 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'user.Users'
+
+
+
+SWAGGER_SETTINGS = {
+    'DEFAULT_API_URL' : os.environ.get('SWAGGER_DEFAULT_API_URL', ""),
+    'USE_SESSION_AUTH': False,
+    'SECURITY_DEFINITIONS': {
+        'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header'
+        }
+    },
+    
+}
--- e_commerce_core/urls.py
+++ e_commerce_core/urls.py
@@ -15,10 +15,50 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+from django.views.generic import RedirectView
+from drf_yasg import openapi
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title= 'Base',
+        default_version= 'v1',
+        description= 'learning nextjs with tailwind'
+    ),
+    public=True,
+    permission_classes = [permissions.AllowAny],
+
+)
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('apps.Auth.urls')),
-    path('users',include('apps.user.urls'))
+    path('', RedirectView.as_view(url='api/docs/',)),
+    
+    re_path(r'^api/',include([
+        path('auth/',include('apps.Auth.urls')),
+        path('users/',include('apps.user.urls')),
+
+
+
+        re_path(r'^docs/', include([
+
+            path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+            path("redoc", schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+        ]))
+    ]))
+
 ]
+
+urlpatterns += staticfiles_urlpatterns()   
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
