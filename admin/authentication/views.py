a30cdb5f4ddbe5cb45f45e59f5923a5a82101fdd
shmnd <hamzashamnad123@gmail.com>
2026-02-03 12:27:32 +0530

    url changes

--- admin/authentication/views.py
+++ admin/authentication/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+class AdminCreateOrUpdateUserApiView():
+
+    def post(self,request):
+        return Response({"message":"User created successfully"},status=status.HTTP_201_CREATED)
